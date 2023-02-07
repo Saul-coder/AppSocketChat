11385cce6f5fa35c9133959e753df7d155b48a80
Saul R. J. C <rojas_cervantes@hotmail.com>
2023-02-07 16:18:45 -0600

    Ejercisio completado

--- public/main.js
+++ public/main.js
@@ -1,4 +1,27 @@
 var socket = io.connect('http://localhost:8080',{'forceNew':true});
+
 socket.on('messages',function(data){
     console.log(data);
+    render(data);
 });
+
+function render(dato){
+    var html = dato.map(function(elem, index){
+        return(`<div>
+        <strong>${elem.author}</stronge>;
+        <em>${elem.text}</em>
+        </div>`);
+    }).join(" ");
+    
+    document.getElementById('messages').innerHTML=html;
+};
+
+function addMessages(e){
+    var payload = {
+        author: document.getElementById('username').value,
+        texto: document.getElementById('texto').value
+    };
+
+    socket.emit('new-Message',payload);
+    return false;
+}
--- server/main.js
+++ server/main.js
@@ -3,21 +3,28 @@ var app= express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var message = [{
+    id:1,
+    text: "Hola soy un mensaje",
+    author: "Saul Antonio"
+}];
+
 app.use(express.static('public'));
 
-app.get('/hello', function(re, res){
+app.get('/hello', function(req, res){
     res.status(200).send("Hello World!");
-})
+});
 
 io.on('connection',function(socket){
     console.log('Alguien se ha conectado con socket');
-    socket.emit('messages',{
-        id:1,
-        text: "Hola soy un mensaje",
-        author: "Saul Antonio"
-    })
-})
+    socket.emit('messages',message);
 
-server.listen(8080,function() {
+    socket.on('new-Message', function(data){
+        message.push(data);
+        io.emit('messages',message);
+    });
+});
+
+server.listen(8080,'0.0.0.0',function() {
     console.log("Servidor corriendo en http://localhost:8080");
-})
+});
